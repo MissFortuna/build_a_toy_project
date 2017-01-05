5c16e974517a22d6fe21eaa10cfe0527b6d067cc
MissFortuna <elizabeth.lemeshko@gmail.com>
2017-01-05 17:12:47 +0200

    Shop page, elements
    Added ejs pages, added shop elements

---
+++ Backend/data/Shop_List.js
@@ -0,0 +1,35 @@
+/**
+ * Created by Elizabeth on 1/5/2017.
+ */
+
+
+
+var dresses_info = [
+    {
+        id:1,
+        icon:'',
+        title: "Платтячко з льону",
+        cloth_type:'Плаття',
+        type: 'Одяг для ляльки',
+        price: 19,
+
+        is_new:true,
+        is_popular:true
+
+    },
+    {
+        id:2,
+        icon:'',
+        title: "Панир чорний",
+        cloth_type:'Панир',
+        type: 'Одяг для черепашки',
+//        content: {
+//            meat: ['балик', 'салямі'],
+//        },
+        price: 29,
+
+        is_popular:true
+    }
+];
+
+module.exports = dresses_info;
